9b7960f9855490e49c0f2e55d0a49e6f70a0acb0
Zachary Murphy <ztmurphy21@gmail.com>
2017-03-27 17:41:53 -0400

    first assignment

---
+++ Product Solution/Product Project/Main Form.vb
@@ -0,0 +1,37 @@
+﻿' Name:         Product Project
+' Purpose:      Displays the product IDs entered by the user
+' Programmer:   Zach Murphy on 3-27-2017
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub txtId_Enter(sender As Object, e As EventArgs) Handles txtId.Enter
+        txtId.SelectAll()
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        ' adds a product ID to a list
+
+        Dim strId As String
+
+        ' remove any leading and trailing spaces
+        strId = txtId.Text.Trim
+        ' verify length
+        If strId.Length = 5 Then
+            lstId.Items.Add(strId.ToUpper)
+        Else
+            MessageBox.Show("The ID must contain 5 characters.",
+                            "Product ID", MessageBoxButtons.OK,
+                            MessageBoxIcon.Information)
+        End If
+        txtId.Focus()
+    End Sub
+
+End Class
