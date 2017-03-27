d05a0ca68bf30e9c94796c6e385a0498a3457335
Zachary Murphy <ztmurphy21@gmail.com>
2017-03-27 17:55:13 -0400

    assignment 3

---
+++ City State Solution/City State Project/Main Form.vb
@@ -0,0 +1,36 @@
+﻿' Name:         City State Project
+' Purpose:      Displays the index of the comma in a string
+' Programmer:   Zach Murphy
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub txtCityState_Enter(sender As Object, e As EventArgs) Handles txtCityState.Enter
+        txtCityState.SelectAll()
+    End Sub
+
+    Private Sub txtCityState_TextChanged(sender As Object, e As EventArgs) Handles txtCityState.TextChanged
+        lblCommaIndex.Text = String.Empty
+    End Sub
+
+    Private Sub btnLocate_Click(sender As Object, e As EventArgs) Handles btnLocate.Click
+        ' displays the index of the comma in a string
+
+        Dim strCityState As String
+        Dim intCommaIndex As Integer
+
+        strCityState = txtCityState.Text
+        ' determine the comma's index
+        intCommaIndex = strCityState.IndexOf(",")
+        lblCommaIndex.Text = intCommaIndex.ToString
+        txtCityState.Focus()
+    End Sub
+
+End Class
