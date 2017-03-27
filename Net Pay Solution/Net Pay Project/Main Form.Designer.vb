eb6ab4dff0e6bca7d3d3b049033a3267a245dab0
Zachary Murphy <ztmurphy21@gmail.com>
2017-03-27 17:45:04 -0400

    assignment 2

---
+++ Net Pay Solution/Net Pay Project/Main Form.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNetPay = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblFormatted = New System.Windows.Forms.Label()
+        Me.btnFormat = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(110, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "&Enter the net pay:"
+        '
+        'txtNetPay
+        '
+        Me.txtNetPay.Location = New System.Drawing.Point(137, 31)
+        Me.txtNetPay.Name = "txtNetPay"
+        Me.txtNetPay.Size = New System.Drawing.Size(59, 25)
+        Me.txtNetPay.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(118, 17)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Formatted net pay:"
+        '
+        'lblFormatted
+        '
+        Me.lblFormatted.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblFormatted.Location = New System.Drawing.Point(137, 77)
+        Me.lblFormatted.Name = "lblFormatted"
+        Me.lblFormatted.Size = New System.Drawing.Size(93, 26)
+        Me.lblFormatted.TabIndex = 5
+        Me.lblFormatted.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnFormat
+        '
+        Me.btnFormat.Location = New System.Drawing.Point(255, 31)
+        Me.btnFormat.Name = "btnFormat"
+        Me.btnFormat.Size = New System.Drawing.Size(73, 33)
+        Me.btnFormat.TabIndex = 2
+        Me.btnFormat.Text = "&Format"
+        Me.btnFormat.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(255, 70)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(73, 33)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(352, 138)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnFormat)
+        Me.Controls.Add(Me.lblFormatted)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtNetPay)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Net Pay"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtNetPay As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblFormatted As System.Windows.Forms.Label
+    Friend WithEvents btnFormat As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Net Pay Solution/Net Pay Project/Main Form.vb
@@ -0,0 +1,51 @@
+﻿' Name:         Net Pay Project
+' Purpose:      Display the net pay with a dollar sign, two decimal places, and asterisks
+' Programmer:   Zach Murphy on 3-27-2017
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub txtNetPay_Enter(sender As Object, e As EventArgs) Handles txtNetPay.Enter
+        txtNetPay.SelectAll()
+    End Sub
+
+    Private Sub txtNetPay_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNetPay.KeyPress
+        ' accept only numbers, the period, and the Backspace
+
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso
+            e.KeyChar <> "." AndAlso e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtNetPay_TextChanged(sender As Object, e As EventArgs) Handles txtNetPay.TextChanged
+        lblFormatted.Text = String.Empty
+    End Sub
+
+    Private Sub btnFormat_Click(sender As Object, e As EventArgs) Handles btnFormat.Click
+        ' format the net pay with two decimal places, then 
+        ' pad with asterisks and insert a dollar sign as the 
+        ' first character
+
+        Dim decNet As Decimal
+        Dim strFormatted As String
+
+        Decimal.TryParse(txtNetPay.Text, decNet)
+        ' format the net pay with two decimal places
+        strFormatted = decNet.ToString("n2")
+        ' pad the net pay with asterisks until its length is 10
+        strFormatted = strFormatted.PadLeft(10, "*"c)
+        ' insert a dollar sign as the first character
+        strFormatted = strFormatted.Insert(0, "$")
+        ' display the net pay, then set the focus
+        lblFormatted.Text = strFormatted
+        txtNetPay.Focus()
+    End Sub
+End Class
