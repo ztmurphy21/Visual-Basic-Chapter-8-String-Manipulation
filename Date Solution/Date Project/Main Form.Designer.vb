a9e4fad037afb426bbd33957a77605bc9a25d5ab
Zachary Murphy <ztmurphy21@gmail.com>
2017-03-27 20:41:45 -0400

    can't get date project to work ugh

---
+++ Date Solution/Date Project/Main Form.Designer.vb
@@ -0,0 +1,113 @@
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Public Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtDate = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblDate = New System.Windows.Forms.Label()
+        Me.btnChange = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(25, 35)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(110, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "&Date (mm/dd/yy):"
+        '
+        'txtDate
+        '
+        Me.txtDate.Location = New System.Drawing.Point(28, 54)
+        Me.txtDate.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtDate.Name = "txtDate"
+        Me.txtDate.Size = New System.Drawing.Size(96, 25)
+        Me.txtDate.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(148, 35)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(122, 17)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Date (mm/dd/yyyy):"
+        '
+        'lblDate
+        '
+        Me.lblDate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblDate.Location = New System.Drawing.Point(151, 54)
+        Me.lblDate.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblDate.Name = "lblDate"
+        Me.lblDate.Size = New System.Drawing.Size(119, 26)
+        Me.lblDate.TabIndex = 5
+        Me.lblDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnChange
+        '
+        Me.btnChange.Location = New System.Drawing.Point(28, 105)
+        Me.btnChange.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnChange.Name = "btnChange"
+        Me.btnChange.Size = New System.Drawing.Size(96, 31)
+        Me.btnChange.TabIndex = 2
+        Me.btnChange.Text = "&Change Date"
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(151, 105)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(96, 31)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "E&xit"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(290, 173)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnChange)
+        Me.Controls.Add(Me.lblDate)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtDate)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Date"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtDate As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblDate As System.Windows.Forms.Label
+    Friend WithEvents btnChange As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Date Solution/Date Project/Main Form.vb
@@ -0,0 +1,50 @@
+' Name:         Date Project
+' Purpose:      Verifies a date's format and changes the year number 
+'               from "yy" to "20yy"
+' Programmer:   Zach Murphy on 3-27-2017
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+    Private Sub btnChange_Click(sender As Object, e As EventArgs) Handles btnChange.Click
+        Dim num As Integer
+        Dim dateNumbers As String
+        Dim finalDate As String
+        Dim dateArray() As String
+
+        Integer.TryParse(txtDate.Text, num)
+        dateNumbers = num.ToString
+        If dateNumbers Like "##[/]##[/]##" Then
+            dateArray = Split(dateNumbers, "/")
+            finalDate = dateArray(0) & "/" + dateArray(1) & "/20" & dateArray(2)
+            lblDate.Text = finalDate
+        End If
+
+
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub txtDate_Enter(sender As Object, e As EventArgs) Handles txtDate.Enter
+        txtDate.SelectAll()
+    End Sub
+
+    Private Sub txtDate_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtDate.KeyPress
+
+
+
+        ' allows only numbers, the slash, and the Backspace key
+
+
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso
+            e.KeyChar <> "/" AndAlso e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+            lblDate.Text = inputDate
+        End If
+    End Sub
+End Class
+
